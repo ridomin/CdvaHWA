af53a6f13e2de4a5c04f688bacdeab7eda8c81e4
rido <rmpablos@hotmail.com>
2015-11-13 12:59:43 -0800

    initial commit

---
+++ CordovaHostedApp/scripts/StartHostedWebApp.ts
@@ -0,0 +1,34 @@
+﻿module CordovaHostedApp {
+    "use strict";
+    export module StartHostedWebApp {
+        var oReq = new XMLHttpRequest();
+        export function checkConnectionAndRedirect (){
+            
+
+            //oReq.addEventListener("progress", updateProgress);
+            oReq.addEventListener("load", transferComplete);
+            oReq.addEventListener("error", transferFailed);
+            oReq.addEventListener("abort", transferCanceled);
+
+            oReq.open("GET", "https://cordovahostedweb.azurewebsites.net/");
+            oReq.send();
+        }
+        function transferComplete(data) {
+            var targetUrl: string = "https://cordovahostedweb.azurewebsites.net/cordova/setPlatformCookie?platform=" + cordova.platformId; 
+
+            window.location.replace(targetUrl);
+
+        }
+
+        function transferFailed(err) {
+            console.log(err.target.reason);
+            console.log(oReq.status);
+            
+        }
+
+        function transferCanceled(data) {
+            console.log(data.target.reason);
+        }
+        
+    }
+}
