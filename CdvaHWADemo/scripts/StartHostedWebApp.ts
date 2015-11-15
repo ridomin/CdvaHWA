82914edc89c88aa4cdf24ff054c0ed07f5c2840a
rido <ridomin@outlook.com>
2015-11-14 23:18:55 -0800

    cleanup scripts

---
+++ CdvaHWADemo/scripts/StartHostedWebApp.ts
@@ -0,0 +1,37 @@
+﻿module CordovaHostedApp {
+    "use strict";
+    export module StartHostedWebApp {
+        const hwa_server: string = "https://cdvahwa.azurewebsites.net/";
+        const hwa_launcher: string = "index.html?platform=";
+        var oReq = new XMLHttpRequest();
+        export function checkConnectionAndRedirect (){
+            
+
+            //oReq.addEventListener("progress", updateProgress);
+            oReq.addEventListener("load", transferComplete);
+            oReq.addEventListener("error", transferFailed);
+            oReq.addEventListener("abort", transferCanceled);
+
+            oReq.open("HEAD", hwa_server);
+            oReq.send();
+        }
+        function transferComplete(data) {
+            var targetUrl: string = hwa_server + hwa_launcher + cordova.platformId; 
+            var bkpLink: HTMLAnchorElement = <HTMLAnchorElement>document.getElementById("bkpLink");
+            bkpLink.setAttribute("href", targetUrl);
+            window.location.replace(targetUrl);
+
+        }
+
+        function transferFailed(err) {
+            console.log(err.target.reason);
+            console.log(oReq.status);
+            
+        }
+
+        function transferCanceled(data) {
+            console.log(data.target.reason);
+        }
+        
+    }
+}
